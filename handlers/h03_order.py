09ff27f6807e81d4d6cebf87ae156f1dec98c0a4
Kirill98834 <kirill_sm8@mail.ru>
2026-02-06 20:18:52 +0300

    Добавление смысла к кнопке Вернуться в главное меню

--- handlers/h03_order.py
+++ handlers/h03_order.py
@@ -1,7 +1,9 @@
 from aiogram import Router, F, Bot
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import Message, FSInputFile
 
 from database.utils import db_get_last_orders
+from handlers.h02_get_contact import show_main_menu
 from keyboards.inline import generate_category_menu
 from keyboards.reply import back_to_main_menu
 
@@ -32,3 +34,14 @@ async def order_history(message: Message):
         line_price = float(order.final_price)
         text += f"{order.product_name} - {order.quantity}шт. - {line_price} руб. \n\n"
     await message.answer(text)
+
+
+@router.message(F.text == "Главное меню")
+async def handle_main_menu(message: Message, bot: Bot):
+    """Обработка кнопки Главное меню и удаление предыдущего сообщения"""
+    try:
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id-1)
+    except TelegramBadRequest:
+        pass
+    await show_main_menu(message)
+
